3718bc1f672d4cc470ca5925bf9abfcb49f42f90
Vincent Le Goff <vincent.le-goff@simple-it.fr>
2013-01-03 17:40:28 +0100

    Add some theoric code for the plugin's sytem

---
+++ ext/aboard/plugin/plugin.py
@@ -0,0 +1,45 @@
+# Copyright (c) 2013 LE GOFF Vincent
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# * Neither the name of the copyright holder nor the names of its contributors
+#   may be used to endorse or promote products derived from this software
+#   without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+# OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+
+"""Module containing the abstract Plugin class."""
+
+class Plugin:
+    
+    """Parent class for all plugins.
+    
+    To create a plugin, a user must inherit from this class.  A
+    plugin is a set of code which is designed to extend the abilities
+    of the Python Aboard framework, by using other libraries for
+    instance.
+    
+    In order to modify the behaviour of Python Aboard, a plugin uses
+    events:  it can subscribe to listen and be called when a specific
+    action occurs.  For instance, a plugin could be called any time
+    a request is sent to the server.
+    
+    """
